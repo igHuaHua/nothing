acbff8ed0f67b8160c1703af339371b7135a349c
igHuaHua <282408643@qq.com>
2020-04-04 12:49:46 +0800

    初次提交

---
+++ zanhu/qa/apps.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+from django.apps import AppConfig
+
+
+class QaConfig(AppConfig):
+    name = 'zanhu.qa'
+    verbose_name = '问答'
---
+++ zanhu/search/apps.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+from django.apps import AppConfig
+
+
+class SearchConfig(AppConfig):
+    name = 'zanhu.search'
+    verbose_name = '搜索'
